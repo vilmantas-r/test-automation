48bf4297734aa2b11c9433f2cb4df12ed4e62fa6
Vilmantas Runas <vilmantas.runas@nortal.com>
2023-07-03 15:12:01 +0300

    Update workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,6 @@ class MissingRequiredPropertyException(envVarName: String, propName: String) : G
 fun Properties.getRequiredProperty(envVarName: String, propName: String) =
     System.getenv(envVarName).takeUnless { it.isNullOrBlank() }
         ?: this.getProperty(propName).takeUnless { it.isNullOrBlank() }
-        ?: throw MissingRequiredPropertyException(envVarName, propName)
 
 val props = Properties()
 rootProject.file("gradle-local.properties").takeIf { it.exists() }?.inputStream()?.use { props.load(it) }
@@ -227,9 +226,9 @@ configure(subprojects.filter { it.name !in setOf("demos", "demo-ui-test") }) {
         repositories {
             maven {
                 url = if (version.toString().endsWith("SNAPSHOT")) {
-                    uri(props.getRequiredProperty("MAVEN_SNAPSHOTS_REPOSITORY_URL", "snapshotsRepoUrl"))
+                    uri("https://oss.sonatype.org/content/repositories/snapshots/")
                 } else {
-                    uri(props.getRequiredProperty("MAVEN_RELEASES_REPOSITORY_URL", "releasesRepoUrl"))
+                    uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
                 }
                 credentials(org.gradle.api.artifacts.repositories.PasswordCredentials::class) {
                     username = props.getRequiredProperty("OSS_USERNAME", "ossUsername")
@@ -240,6 +239,10 @@ configure(subprojects.filter { it.name !in setOf("demos", "demo-ui-test") }) {
     }
 
     signing {
+        val signingKeyId: String? by project
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
         sign(publishing.publications[project.name])
     }
 }
